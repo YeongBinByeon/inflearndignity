82e0b5ffa091b7a2966e93ff78477d102fbb2f47
yeongbin <yeongbin6483@gmail.com>
2026-03-28 17:18:14 +0900

    js/41_class까지

---
+++ javascript/js/log.js
@@ -0,0 +1,11 @@
+export function log(message) {
+  console.log(message);
+}
+
+export function error(message) {
+  console.error(message);
+}
+
+export function warning(message) {
+  console.warn(message);
+}
